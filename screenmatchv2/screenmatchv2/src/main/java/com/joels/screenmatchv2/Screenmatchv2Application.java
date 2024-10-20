ffc2a4d0541391a95cd1dd34cf1b9335a8d4bcf2
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-10-20 11:28:49 -0500

    Create spring proyect screenmatchv2

---
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/Screenmatchv2Application.java
@@ -0,0 +1,20 @@
+package com.joels.screenmatchv2;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Screenmatchv2Application implements CommandLineRunner{
+
+	public static void main(String[] args) {
+		SpringApplication.run(Screenmatchv2Application.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
