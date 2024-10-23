f22e5dff6a6f8e31cebec62b8543fbeaf437ca15
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-23 16:42:06 -0500

    Create DataEpisode

---
+++ screenmatchv2/screenmatchv2/src/main/java/model/DataEpisode.java
@@ -0,0 +1,8 @@
+package model;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown =  true)
+public record DataEpisode(@JsonAlias("Title") String title,@JsonAlias("Episode") Integer episodeNumber,@JsonAlias("imdbRating") String imdbRating,@JsonAlias("Released") String released) {
+
+}
