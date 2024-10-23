a0328e4cbb66ee8011a9e31bc2a491cc0f5b47ef
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-23 16:41:32 -0500

    Create Main

---
+++ screenmatchv2/screenmatchv2/src/main/java/main/Main.java
@@ -0,0 +1,38 @@
+package main;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+import model.DataSeason;
+import model.DataSerie;
+import service.ConvertData;
+import service.ServiceAPI;
+
+public class Main {
+
+	private Scanner keyboard = new Scanner(System.in);
+	private ServiceAPI serviceApi = new ServiceAPI();
+	private final String URL_BASE = "http://www.omdbapi.com/?t=";
+	private final String API_KEY="&apikey=9f0dabfa";
+	private ConvertData converter = new ConvertData();
+	
+	
+	public void showMenu() {
+		System.out.println("Enter de series name: ");
+		//search for general data of the series
+		var serieName = keyboard.nextLine();
+		var json = serviceApi.getData(URL_BASE+ serieName.replace(" ", "+") + API_KEY);
+		var data = converter.getData(json, DataSerie.class);
+		System.out.println(data);
+		//Search data by season
+		List<DataSeason> seasons = new ArrayList<>();
+		for(int i = 1;i <= data.totalSeasons(); i++) {
+			json = serviceApi.getData(URL_BASE+ serieName.replace(" ", "+") + "&Season="+ i + API_KEY);
+			var dataSeason = converter.getData(json, DataSeason.class);
+			seasons.add(dataSeason);
+		}
+		
+		seasons.forEach(System.out::println);
+	}
+}
