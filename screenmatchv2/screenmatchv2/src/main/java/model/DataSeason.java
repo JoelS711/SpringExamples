2b0726559918cfc9c1d33898923b7f84924265f4
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-23 16:42:36 -0500

    Create DataSeason

---
+++ screenmatchv2/screenmatchv2/src/main/java/model/DataSeason.java
@@ -0,0 +1,11 @@
+package model;
+
+import java.util.List;
+
+import com.fasterxml.jackson.annotation.JsonAlias;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+@JsonIgnoreProperties(ignoreUnknown =  true)
+public record DataSeason(@JsonAlias("Season") Integer numero, @JsonAlias("Episodes") List<DataEpisode> episodes) {
+
+}
