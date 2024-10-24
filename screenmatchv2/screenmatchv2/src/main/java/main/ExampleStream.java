26fca52a367fd403a0868c5660e513eaf9d34276
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-24 09:36:59 -0500

    Create ExampleStream

---
+++ screenmatchv2/screenmatchv2/src/main/java/main/ExampleStream.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExampleStream {
+
+	public void showExample() {
+		List<String> names = Arrays.asList("Camilo", "Juan", "Mauricio", "Duvan","Fabian", "Diego");
+		names.stream().sorted().limit(4).filter(t -> t.startsWith("D")).map(n -> n.toUpperCase()).forEach(System.out::println);
+	}
+}
