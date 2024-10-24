57950167315da6be0673300b0294721eb61f3241
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-24 09:37:38 -0500

    Update Screenmatchv2Application

--- screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/Screenmatchv2Application.java
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/Screenmatchv2Application.java
@@ -6,6 +6,7 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import main.ExampleStream;
 import main.Main;
 @SpringBootApplication
 public class Screenmatchv2Application implements CommandLineRunner{
@@ -17,9 +18,10 @@ public class Screenmatchv2Application implements CommandLineRunner{
 	@Override
 	public void run(String... args) throws Exception {
 		
-		Main main = new Main();
-		main.showMenu();
-		
+		//Main main = new Main();
+		//main.showMenu();
+		ExampleStream exampleStream = new ExampleStream();
+		exampleStream.showExample();
 	}
 
 }
