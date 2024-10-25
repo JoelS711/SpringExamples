3f60963c1a32ea2621b9337b6eb9adbed4aee8ed
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-25 16:19:50 -0500

    Update Data.java

--- apiBook/apiBook/src/main/java/model/Data.java
+++ apiBook/apiBook/src/main/java/model/Data.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 @JsonIgnoreProperties(ignoreUnknown = true)
-public record Data(@JsonAlias("results") List<Data> books) {
+public record Data(@JsonAlias("results") List<DataBook> books) {
 
 	
 }
