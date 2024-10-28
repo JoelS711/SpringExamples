2e5e0d3479f77bfc0e2bc7a84442ed0de7cd3ff2
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-28 15:48:43 -0500

    Update DataSerie.java

--- screenmatchv2/screenmatchv2/src/main/java/model/DataSerie.java
+++ screenmatchv2/screenmatchv2/src/main/java/model/DataSerie.java
@@ -3,7 +3,9 @@ package model;
 import com.fasterxml.jackson.annotation.JsonAlias;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
-@JsonIgnoreProperties(ignoreUnknown =  true)
-public record DataSerie(@JsonAlias("Title") String title, @JsonAlias("totalSeasons") Integer totalSeasons, @JsonAlias("imdbRating") String rating) {
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record DataSerie(@JsonAlias("Title") String title, @JsonAlias("totalSeasons") Integer totalSeasons,
+		@JsonAlias("imdbRating") String rating, @JsonAlias("Poster") String poster, @JsonAlias("Genre") String genre,
+		@JsonAlias("Actors") String actors, @JsonAlias("Plot") String sypnosis) {
 
 }
