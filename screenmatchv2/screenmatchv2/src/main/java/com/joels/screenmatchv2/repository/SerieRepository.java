5e79ab883f36981fa3751226fdcdf70e1f400272
JoelS711 <81266779+JoelS711@users.noreply.github.com>
2024-10-30 22:28:40 -0500

    Create package repository

---
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/repository/SerieRepository.java
@@ -0,0 +1,9 @@
+package com.joels.screenmatchv2.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.joels.screenmatchv2.model.Serie;
+
+public interface SerieRepository extends JpaRepository<Serie, Long>{
+
+}
