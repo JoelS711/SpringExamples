57ceb7f8ac8e07f9e002f4a31668a6e29bef76a4
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-02 16:59:23 -0500

    Update and add methods SerieRepository.java

--- screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/repository/SerieRepository.java
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/repository/SerieRepository.java
@@ -4,11 +4,26 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.joels.screenmatchv2.model.Category;
+import com.joels.screenmatchv2.model.Episode;
 import com.joels.screenmatchv2.model.Serie;
 
 public interface SerieRepository extends JpaRepository<Serie, Long> {
 
 	Optional<Serie> findByTitleContainsIgnoreCase(String nameSerie);
-	List<Serie> findTop5ByOrderByRating();
+	
+	List<Serie> findTop5ByOrderByRatingDesc();
+	
+	List<Serie> findByGenre(Category category);
+	
+	@Query("SELECT s FROM Serie s WHERE s.totalSeasons <= :totalSeasons AND s.rating >= :rating")
+	List<Serie> serieBySeasonAndRating(int totalSeasons, Double rating);
+	
+	@Query("SELECT e FROM Serie s JOIN s.episodes e WHERE e.title ILIKE %:nameEpisode%")
+	List<Episode> episodeXName(String nameEpisode);
+	
+	@Query("SELECT e FROM Serie s JOIN s.episodes e WHERE s = :serie ORDER BY e.rating DESC LIMIT 5")
+	List<Episode> top5Episodes(Serie serie);
 }
