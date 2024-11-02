a092f2f180cf1dbf9533efe5601d425371dafee1
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-02 12:38:18 -0500

    Update SerieRepository.java

--- screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/repository/SerieRepository.java
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/repository/SerieRepository.java
@@ -1,9 +1,14 @@
 package com.joels.screenmatchv2.repository;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.joels.screenmatchv2.model.Serie;
 
-public interface SerieRepository extends JpaRepository<Serie, Long>{
+public interface SerieRepository extends JpaRepository<Serie, Long> {
 
+	Optional<Serie> findByTitleContainsIgnoreCase(String nameSerie);
+	List<Serie> findTop5ByOrderByRating();
 }
