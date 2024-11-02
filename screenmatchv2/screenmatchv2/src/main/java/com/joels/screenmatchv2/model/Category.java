8ad294bb4f9e7d83c33d2242642420bda1ce799e
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-02 16:57:48 -0500

    Update Category

--- screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/model/Category.java
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/model/Category.java
@@ -2,21 +2,32 @@ package com.joels.screenmatchv2.model;
 
 public enum Category {
 
-	ACTION("Action"),
-	ROMANCE("Romance"),
-	COMEDY("Comedy"),
-	CRIME("Crime"),
-	DRAMA("Drama");
+	ACTION("Action","Acción"),
+	ROMANCE("Romance","Romance"),
+	COMEDY("Comedy","Comedia"),
+	CRIME("Crime","Crimen"),
+	DRAMA("Drama", "Drama");
 	
 	private String categoryOmdb;
+	private String categorySpanish;
 	
-	Category (String categoryOmdb){
+	Category (String categoryOmdb, String categorySpanish){
 		this.categoryOmdb = categoryOmdb;
+		this.categorySpanish = categorySpanish;
 	}
 	
 	public static Category fromString(String text) {
 		for(Category category : Category.values()) {
 			if(category.categoryOmdb.equalsIgnoreCase(text)) {
+				return category;
+			}
+		}
+		throw new IllegalArgumentException("no category found" +text);
+	}
+	
+	public static Category fromSpanish(String text) {
+		for(Category category : Category.values()) {
+			if(category.categorySpanish.equalsIgnoreCase(text)) {
 				return category;
 			}
 		}
