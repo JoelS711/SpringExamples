c2e339fa2c89583cca3f02f071104c5aa75d8a23
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-05 15:55:46 -0500

    Update Screenmatchv2Application.java

--- screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/Screenmatchv2Application.java
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/Screenmatchv2Application.java
@@ -1,34 +1,18 @@
 package com.joels.screenmatchv2;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.joels.screenmatchv2.main.Principal;
-import com.joels.screenmatchv2.repository.SerieRepository;
 
 @SpringBootApplication
-public class Screenmatchv2Application implements CommandLineRunner {
+public class Screenmatchv2Application {
 
 	
-	@Autowired
-	private SerieRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(Screenmatchv2Application.class, args);
 		
-	}
-
-	@Override
-	public void run(String... args) throws Exception {
-
-		// Main main = new Main();
-		// main.showMenu();
-		// ExampleStream exampleStream = new ExampleStream();
-		// exampleStream.showExample();
-		Principal principal = new Principal(repository);
-		principal.showMenu();
-
 	}
 
 }
