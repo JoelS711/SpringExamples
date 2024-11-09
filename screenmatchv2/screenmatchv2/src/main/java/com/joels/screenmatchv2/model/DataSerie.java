69f71be1590ef6d02bd8ddf534352f2e7491be8d
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-09 13:47:44 -0500

    Update DataSerie.java

--- screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/model/DataSerie.java
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/model/DataSerie.java
@@ -6,6 +6,6 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 @JsonIgnoreProperties(ignoreUnknown = true)
 public record DataSerie(@JsonAlias("Title") String title, @JsonAlias("totalSeasons") Integer totalSeasons,
 		@JsonAlias("imdbRating") String rating, @JsonAlias("Poster") String poster, @JsonAlias("Genre") String genre,
-		@JsonAlias("Actors") String actors, @JsonAlias("Plot") String sypnosis) {
+		@JsonAlias("Actors") String actors, @JsonAlias("Plot") String synopsis) {
 
 }
