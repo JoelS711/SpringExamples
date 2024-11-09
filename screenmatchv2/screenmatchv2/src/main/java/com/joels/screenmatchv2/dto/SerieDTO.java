3868b391cfa5d2573a6044ba7e5b889f5e1ce86e
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-09 13:47:07 -0500

    Update SerieDTO.java

--- screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/dto/SerieDTO.java
+++ screenmatchv2/screenmatchv2/src/main/java/com/joels/screenmatchv2/dto/SerieDTO.java
@@ -2,16 +2,7 @@ package com.joels.screenmatchv2.dto;
 
 import com.joels.screenmatchv2.model.Category;
 
-
-
-public record SerieDTO(
-		 String title,
-		 Integer totalSeasons,
-		 Double rating,
-		 String poster,
-		 Category genre,
-		 String actors,
-		 String sypnosis
-		) {
+public record SerieDTO(Long id, String title, Integer totalSeasons, Double rating, String poster, Category genre,
+		String actors, String synopsis) {
 
 }
