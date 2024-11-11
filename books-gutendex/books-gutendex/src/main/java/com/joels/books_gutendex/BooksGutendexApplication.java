d1771f5f5e3b7d26546115941172d421472c2a17
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-11 08:37:21 -0500

    Create project books-gutendex

---
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/BooksGutendexApplication.java
@@ -0,0 +1,13 @@
+package com.joels.books_gutendex;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BooksGutendexApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BooksGutendexApplication.class, args);
+	}
+
+}
---
+++ books-gutendex/books-gutendex/src/test/java/com/joels/books_gutendex/BooksGutendexApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joels.books_gutendex;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BooksGutendexApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
