bc473078b867af9504af869e0e92b7d7e71f5be9
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-12 16:43:00 -0500

    Create package model and class Book.java

---
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/model/Book.java
@@ -0,0 +1,56 @@
+package com.joels.books_gutendex.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "books")
+public class Book {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long Id;
+	
+	private String title;
+	private String author;
+	private String language;
+	private Long downloads;
+	
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getTitle() {
+		return title;
+	}
+	public void setTitle(String title) {
+		this.title = title;
+	}
+	
+	public String getAuthor() {
+		return author;
+	}
+	public void setAuthor(String author) {
+		this.author = author;
+	}
+	public String getLanguage() {
+		return language;
+	}
+	public void setLanguage(String language) {
+		this.language = language;
+	}
+	public Long getDownloads() {
+		return downloads;
+	}
+	public void setDownloads(Long downloads) {
+		this.downloads = downloads;
+	}
+	
+	
+}
