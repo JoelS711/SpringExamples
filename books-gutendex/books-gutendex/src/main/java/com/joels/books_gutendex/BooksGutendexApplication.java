e5cc456e5b5c4320454f5e08cd5e0e4496e7be61
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-15 13:30:31 -0500

    Update BooksGutendexApplication.java

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/BooksGutendexApplication.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/BooksGutendexApplication.java
@@ -1,20 +1,28 @@
 package com.joels.books_gutendex;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import com.joels.books_gutendex.main.Main;
+import com.joels.books_gutendex.repository.BookRepository;
 
 
 @SpringBootApplication
 public class BooksGutendexApplication implements CommandLineRunner{
 
+	@Autowired
+	private BookRepository repository;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BooksGutendexApplication.class, args);
 	}
 	
 	@Override
 	public void run(String... args) throws Exception {
-		
+		Main main = new Main(repository);
+		main.showMenu();
 	}
 
 }
