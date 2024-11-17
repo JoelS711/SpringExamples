6971e0364b07312df7ab6f36a820209f61fe2d96
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-17 14:02:39 -0500

    Update Author.java

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/model/Author.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/model/Author.java
@@ -2,6 +2,7 @@ package com.joels.books_gutendex.model;
 
 import java.util.List;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -10,18 +11,19 @@ import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 
 @Entity
-@Table(name = "authors")
+@Table(name = "author")
 public class Author {
 
 	 	@Id
 	    @GeneratedValue(strategy = GenerationType.IDENTITY)
 	    private Long id;
 
+	 	@Column(unique = true)
 	    private String name;
 	    private Integer birthYear;
 	    private Integer deathYear;
 
-	    @ManyToMany
+	    @ManyToMany(mappedBy = "author")
 	    private List<Book> books;
 
 	    public Author() {
