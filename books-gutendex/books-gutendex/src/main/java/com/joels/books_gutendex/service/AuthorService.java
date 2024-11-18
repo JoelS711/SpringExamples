fe76e57d57212e2764b36b2c54d19a93e24db0e8
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-18 09:08:45 -0500

    Update AuthorService.java

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/service/AuthorService.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/service/AuthorService.java
@@ -1,5 +1,33 @@
 package com.joels.books_gutendex.service;
 
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joels.books_gutendex.model.Author;
+import com.joels.books_gutendex.repository.AuthorRepository;
+
+@Service
 public class AuthorService {
 
+	private final AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+    
+    public Optional<Author> getAuthorByName(String name) {
+        return authorRepository.findByName(name);
+    }
+    
+    public List<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+    
+    public void saveAuthor(Author author) {
+    	authorRepository.save(author);
+    }
 }
