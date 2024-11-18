e3603250c8a51d24bc7aa7b8e1304c36b1df6129
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-18 09:09:11 -0500

    Update Main.java

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/main/Main.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/main/Main.java
@@ -14,23 +14,24 @@ import com.joels.books_gutendex.model.Data;
 import com.joels.books_gutendex.model.DataBook;
 import com.joels.books_gutendex.repository.AuthorRepository;
 import com.joels.books_gutendex.repository.BookRepository;
+import com.joels.books_gutendex.service.AuthorService;
+import com.joels.books_gutendex.service.BookService;
 import com.joels.books_gutendex.service.ConvertData;
 import com.joels.books_gutendex.service.ServiceAPI;
 
 @Component
 public class Main {
 
-	private static final String URL_BASE = "https://gutendex.com/books/";
-	private ServiceAPI serviceApi = new ServiceAPI();
-	private ConvertData converter = new ConvertData();
-	private BookRepository repository;
-	private AuthorRepository authorRepository;
-	private Scanner keyboard = new Scanner(System.in);
 	
-	public Main(BookRepository repository, AuthorRepository authorRepo) {
-		this.repository = repository;
-		this.authorRepository = authorRepo;
-	}
+	private final BookService bookService; 
+    private final AuthorService authorService; 
+    private Scanner keyboard = new Scanner(System.in);
+	
+    @Autowired
+    public Main(BookService bookService, AuthorService authorService) {
+        this.bookService = bookService;
+        this.authorService = authorService;
+    }
 	
 	public void showMenu() {
 		var option = -1;
@@ -50,10 +51,12 @@ public class Main {
 
 			switch (option) {
 			case 1:
-				searchBook();
+				System.out.println("name of the book to search: ");
+				var bookName = keyboard.nextLine();
+				bookService.searchBook(bookName);
 				break;
 			case 2:
-				listRegisteredBooks();
+				//listRegisteredBooks();
 				break;
 			case 3:
 				
@@ -73,50 +76,11 @@ public class Main {
 		}
 	}
 	
-	private DataBook getDataBook() {
-		System.out.println("name of the book to search: ");
-		var bookName = keyboard.nextLine();
-		var json = serviceApi.getData(URL_BASE+"?search="+bookName.replace(" ","+"));
-		Data data;
-		try {
-			  data = converter.getData(json, Data.class);			
-		}catch (Exception e) {
-            throw new RuntimeException("Error parsing JSON", e);
-        }
-		
-		if (data.books() == null || data.books().isEmpty()) {
-	        throw new RuntimeException("No books found in the response");
-	    }
-		
-		var bookSearched = data.books().stream()
-	            .filter(b -> b.title().toUpperCase().contains(bookName.toUpperCase()))
-	            .findFirst()
-	            .orElseThrow(() -> new RuntimeException("Book not found: " + bookName));
-		return bookSearched;
-	}
 	
-	public void searchBook() {
-		try {
-			DataBook data = getDataBook();
-			List<Author> authors = data.author().stream()
-					.map(authorInfo -> {
-						return authorRepository.findByName(authorInfo.name())
-								.orElseGet(() -> {
-									Author newAuthor = new Author(authorInfo);
-									authorRepository.save(newAuthor);
-									return newAuthor;
-								});
-					}).toList();
-			Book book = new Book(data, authors);
-			repository.save(book);
-			System.out.println(book.toString());
-		}catch (Exception e) {
-	        System.out.println("An error occurred while searching for the book: " + e.getMessage());
-	    }
-		
-	}
 	
-	public void listRegisteredBooks() {
+	
+	
+	/*public void listRegisteredBooks() {
 	    List<Book> books = repository.findAll();
 
 	    if (books.isEmpty()) {
@@ -134,5 +98,5 @@ public class Main {
 	            System.out.println("--------------------------------");
 	        });
 	    }
-	}
+	}*/
 }
