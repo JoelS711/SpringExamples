819e478c86f964f0b30598aa81e57c5fcb37dc52
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-18 21:07:56 -0500

    Update ConvertData.java

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/service/ConvertData.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/service/ConvertData.java
@@ -1,8 +1,11 @@
 package com.joels.books_gutendex.service;
 
+import org.springframework.stereotype.Component;
+
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@Component
 public class ConvertData implements IConvertData {
 
 	private ObjectMapper objectMapper = new ObjectMapper();
