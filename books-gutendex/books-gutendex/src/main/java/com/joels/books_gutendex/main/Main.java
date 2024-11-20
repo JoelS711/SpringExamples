7f9651c68e3faa3256d8f9e1fdfa154b5821396d
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-19 21:19:28 -0500

    Update Main.java and add methods

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/main/Main.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/main/Main.java
@@ -1,23 +1,13 @@
 package com.joels.books_gutendex.main;
 
 import java.util.List;
-import java.util.Optional;
 import java.util.Scanner;
-import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
-import com.joels.books_gutendex.model.Author;
-import com.joels.books_gutendex.model.Book;
-import com.joels.books_gutendex.model.Data;
-import com.joels.books_gutendex.model.DataBook;
-import com.joels.books_gutendex.repository.AuthorRepository;
-import com.joels.books_gutendex.repository.BookRepository;
 import com.joels.books_gutendex.service.AuthorService;
 import com.joels.books_gutendex.service.BookService;
-import com.joels.books_gutendex.service.ConvertData;
-import com.joels.books_gutendex.service.ServiceAPI;
 
 @Component
 public class Main {
@@ -59,13 +49,29 @@ public class Main {
 				bookService.listRegisteredBooks();
 				break;
 			case 3:
-				
+				authorService.displayAllAuthors();;
 				break;
 			case 4:
-				
+				System.out.println("Enter the year: ");
+				var year = keyboard.nextInt();
+				authorService.displayAuthorsByYear(year);
 				break;
 			case 5:
-				
+				System.out.println("""
+				        Enter the language to search for books:
+				        Spanish (es)
+				        English (en)
+				        Italian (it)
+				        Portuguese (pt)
+				        Russian (ru)
+				    """);
+				String languageCode = keyboard.nextLine().trim().toLowerCase();
+				List<String> validLanguages = List.of("es", "en", "it", "pt", "ru");
+			    if (!validLanguages.contains(languageCode)) {
+			        System.out.println("Invalid language code. Please try again.");
+			        break;
+			    }
+			    bookService.listBooksByLanguage(languageCode);
 				break;
 			case 0:
 				System.out.println("Exiting...");
