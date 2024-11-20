99212593c43ff94efdf30a7ce9aa70b0a317efc3
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-19 21:20:33 -0500

    Update AuthorRepository.java

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/repository/AuthorRepository.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/repository/AuthorRepository.java
@@ -1,8 +1,11 @@
 package com.joels.books_gutendex.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.joels.books_gutendex.model.Author;
@@ -11,4 +14,7 @@ import com.joels.books_gutendex.model.Author;
 public interface AuthorRepository extends JpaRepository<Author, Long>{
 
 	Optional<Author> findByName(String name);
+	
+	@Query("SELECT a FROM Author a WHERE a.birthYear <= :year AND (a.deathYear > :year OR a.deathYear IS NULL)")
+    List<Author> findAuthorsWithYear(@Param("year") int year);
 }
