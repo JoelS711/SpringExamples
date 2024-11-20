fac88a9307e187b5acadf0caff7c4d5b7f568b06
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-19 21:22:22 -0500

    Update AuthorService.java

--- books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/service/AuthorService.java
+++ books-gutendex/books-gutendex/src/main/java/com/joels/books_gutendex/service/AuthorService.java
@@ -3,7 +3,6 @@ package com.joels.books_gutendex.service;
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.joels.books_gutendex.model.Author;
@@ -14,7 +13,6 @@ public class AuthorService {
 
 	private final AuthorRepository authorRepository;
 
-    @Autowired
     public AuthorService(AuthorRepository authorRepository) {
         this.authorRepository = authorRepository;
     }
@@ -29,5 +27,44 @@ public class AuthorService {
     
     public void saveAuthor(Author author) {
     	authorRepository.save(author);
+    }
+    
+    private void printAuthor(Author author) {
+        System.out.printf("""
+                ------------AUTHOR--------------
+                Id: %s
+                Name: %s
+                Birth Year: %s
+                Death Year: %s
+                --------------------------------
+                """,
+                author.getId(),
+                author.getName(),
+                author.getBirthYear() != null ? author.getBirthYear() : "N/A",
+                author.getDeathYear() != null ? author.getDeathYear() : "N/A"
+        );
+    }
+    
+    public void displayAllAuthors() {
+    	var authors = getAllAuthors();
+    	if(authors.isEmpty()) {
+    		System.out.println("No authors found in the database.");
+            return;
+    	}
+    	authors.forEach(this::printAuthor);
+    }
+    
+    
+    public List<Author> findAuthorByYear(int year){
+    	return authorRepository.findAuthorsWithYear(year);
+    }
+    
+    public void displayAuthorsByYear(int year) {
+    	var authors = findAuthorByYear(year);
+    	if(authors.isEmpty()) {
+    		System.out.println("There are no authors with that time period.");
+    	}else {
+    		authors.forEach(this::printAuthor);
+    	}
     }
 }
