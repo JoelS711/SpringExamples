52b2a970738f181c24c99de005b952ba8c51d4a5
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-22 20:26:55 -0500

    Create package address and classes Address.java and DataAddress.java

---
+++ api/api/src/main/java/joels/medplus/api/address/Address.java
@@ -0,0 +1,19 @@
+package joels.medplus.api.address;
+
+import jakarta.persistence.Embeddable;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Address {
+
+	private String street;
+	private String district;
+	private String city;
+	private Integer number;
+	private String complement;
+}
---
+++ api/api/src/main/java/joels/medplus/api/address/DataAddress.java
@@ -0,0 +1,5 @@
+package joels.medplus.api.address;
+
+public record DataAddress(String street, String district, String city, String number, String complement) {
+
+}
