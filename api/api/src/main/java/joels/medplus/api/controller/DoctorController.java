a6eb5c67dae20321b6eddb36b0c20fa8adab1e07
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-23 07:55:04 -0500

    Update DoctorController.java

--- api/api/src/main/java/joels/medplus/api/controller/DoctorController.java
+++ api/api/src/main/java/joels/medplus/api/controller/DoctorController.java
@@ -1,17 +1,24 @@
 package joels.medplus.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import joels.medplus.api.doctor.DataDoctor;
+import joels.medplus.api.doctor.Doctor;
+import joels.medplus.api.doctor.DoctorRepository;
+
 @RestController
 @RequestMapping("/doctor")
 public class DoctorController {
 
+	@Autowired
+	private DoctorRepository doctorRepository;
+	
 	@PostMapping
-	public void registerDoctor(@RequestBody String param) {
-		System.out.println("The request is correct");
-		System.out.println(param);
+	public void registerDoctor(@RequestBody DataDoctor dataDoctor) {
+		doctorRepository.save(new Doctor(dataDoctor));
 	}
 }
