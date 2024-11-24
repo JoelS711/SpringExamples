cd33d2f09e141e57e828f45b77186fe2b9abfc42
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-24 13:50:45 -0500

    Update DataDoctor.java

--- api/api/src/main/java/joels/medplus/api/doctor/DataDoctor.java
+++ api/api/src/main/java/joels/medplus/api/doctor/DataDoctor.java
@@ -1,11 +1,25 @@
 package joels.medplus.api.doctor;
 
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 import joels.medplus.api.address.DataAddress;
 
-public record DataDoctor(String name,
+public record DataDoctor(@NotBlank String name,
+		@NotBlank
+		@Email
 		String email,
+		@NotBlank
+		String phone,
+		@NotBlank
+		@Pattern(regexp = "\\d{4,6}")
 		String identification,
+		@NotNull
 		Speciality speciality,
+		@NotNull
+		@Valid
 		DataAddress	address) {
 
 }
