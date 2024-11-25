7ff03cf33f84e5c8ea23823f03d92c8755a3ff97
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-25 08:29:03 -0500

    Update HelloController.java

--- api/api/src/main/java/joels/medplus/api/controller/HelloController.java
+++ api/api/src/main/java/joels/medplus/api/controller/HelloController.java
@@ -1,5 +1,12 @@
 package joels.medplus.api.controller;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class HelloController {
 
 }
