c100ec869402520a997d1d341eac3c58452347d8
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-26 12:31:29 -0500

    Update DoctorController.java

--- api/api/src/main/java/joels/medplus/api/controller/DoctorController.java
+++ api/api/src/main/java/joels/medplus/api/controller/DoctorController.java
@@ -1,8 +1,10 @@
 package joels.medplus.api.controller;
 
-import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,8 +12,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.validation.Valid;
-import joels.medplus.api.doctor.DataDoctor;
+import joels.medplus.api.doctor.DataRegisterDoctor;
 import joels.medplus.api.doctor.Doctor;
+import joels.medplus.api.doctor.DoctorListData;
 import joels.medplus.api.doctor.DoctorRepository;
 
 @RestController
@@ -22,12 +25,12 @@ public class DoctorController {
 	private DoctorRepository doctorRepository;
 	
 	@PostMapping
-	public void registerDoctor(@RequestBody @Valid DataDoctor dataDoctor) {
+	public void registerDoctor(@RequestBody @Valid DataRegisterDoctor dataDoctor) {
 		doctorRepository.save(new Doctor(dataDoctor));
 	}
 	
 	@GetMapping
-	public List<Doctor> getAllDoctors(){
-	    return doctorRepository.findAll();
+	public Page<DoctorListData> getAllDoctors(@PageableDefault(size=2) Pageable pageable){
+	    return doctorRepository.findAll(pageable).map(DoctorListData::new);
 	}
 }
