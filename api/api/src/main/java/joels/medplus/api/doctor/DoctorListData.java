f3fa84edcdb3e592dbd9ddfcdf29376d48459c7d
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-11-27 13:21:34 -0500

    Update DoctorListData.java

--- api/api/src/main/java/joels/medplus/api/doctor/DoctorListData.java
+++ api/api/src/main/java/joels/medplus/api/doctor/DoctorListData.java
@@ -1,11 +1,11 @@
 package joels.medplus.api.doctor;
 
 
-public record DoctorListData(String name,String email,
+public record DoctorListData(Long id, String name,String email,
 		String identification,String speciality) {
 
 	public DoctorListData(Doctor doctor) {
-		this(doctor.getName(), doctor.getEmail(), doctor.getIdentification(), doctor.getSpeciality().toString());
+		this(doctor.getId() ,doctor.getName(), doctor.getEmail(), doctor.getIdentification(), doctor.getSpeciality().toString());
 	}
 
 	
