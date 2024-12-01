98dd05f95ceb3bacbbd5acb58bcee9f9b6eae892
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-01 14:42:48 -0500

    Update User.java

--- api/api/src/main/java/joels/medplus/api/domain/user/User.java
+++ api/api/src/main/java/joels/medplus/api/domain/user/User.java
@@ -1,5 +1,12 @@
 package joels.medplus.api.domain.user;
 
+import java.util.Collection;
+import java.util.List;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -16,11 +23,41 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class User {
+public class User implements UserDetails{
 
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String user;
 	private String pass;
+	
+	
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+	}
+	@Override
+	public String getPassword() {
+		return pass;
+	}
+	@Override
+	public String getUsername() {
+		return user;
+	}
+	@Override
+	public boolean isEnabled() {
+		return true;
+	}
+	@Override
+	public boolean isCredentialsNonExpired() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonLocked() {
+		return true;
+	}
+	@Override
+	public boolean isAccountNonExpired() {
+		return true;
+	}
 }
