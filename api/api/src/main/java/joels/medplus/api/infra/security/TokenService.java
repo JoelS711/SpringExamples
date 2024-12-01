dc821fc48ff9d8686aab96b3bdc0d83182306a95
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-01 18:40:37 -0500

    Update TokenService.java

--- api/api/src/main/java/joels/medplus/api/infra/security/TokenService.java
+++ api/api/src/main/java/joels/medplus/api/infra/security/TokenService.java
@@ -36,25 +36,25 @@ public class TokenService {
 	}
 	
 	public String getSubject(String token) {
-		if(token == null) {
-			throw new RuntimeException();
-		}
-		DecodedJWT verifier = null;
-		try {
-		    Algorithm algorithm = Algorithm.HMAC256(apiSecret);
-		    verifier = JWT.require(algorithm)
-		        .withIssuer("medplus")
-		        .build()
-		        .verify(token);
-		    verifier.getSubject();
-		} catch (JWTVerificationException exception){
-		    System.out.println(exception.toString());
-		}
-		if(verifier.getSubject() == null) {
-			throw new RuntimeException("Invalid Verifier");
-		}
-		return verifier.getSubject();
-	}
+        if (token == null) {
+            throw new RuntimeException();
+        }
+        DecodedJWT verifier = null;
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(apiSecret); // validando firma
+            verifier = JWT.require(algorithm)
+                    .withIssuer("medplus")
+                    .build()
+                    .verify(token);
+            verifier.getSubject();
+        } catch (JWTVerificationException exception) {
+            System.out.println(exception.toString());
+        }
+        if (verifier.getSubject() == null) {
+            throw new RuntimeException("invalid verifier");
+        }
+        return verifier.getSubject();
+    }
 	
 	private Instant generateExpirationDate() {
 		return LocalDateTime.now().plusHours(2).toInstant(ZoneOffset.of("-05:00"));
