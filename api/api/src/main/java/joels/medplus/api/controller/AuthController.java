cd4a8c609969d9833170aed3994244e3f5503378
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-01 14:41:46 -0500

    Update AuthController.java

--- api/api/src/main/java/joels/medplus/api/controller/AuthController.java
+++ api/api/src/main/java/joels/medplus/api/controller/AuthController.java
@@ -6,10 +6,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import jakarta.validation.Valid;
 import joels.medplus.api.domain.user.DataAuthUser;
+import joels.medplus.api.domain.user.User;
+import joels.medplus.api.infra.security.DataJWTToken;
+import joels.medplus.api.infra.security.TokenService;
 
 @RestController
 @RequestMapping("/login")
@@ -18,11 +23,15 @@ public class AuthController {
 	@Autowired
 	private AuthenticationManager authenticationManager;
 	
+	@Autowired
+	private TokenService tokenService;
+	
 	@PostMapping
-	public ResponseEntity authUser(DataAuthUser dataAuthUser) {
-		Authentication token = new UsernamePasswordAuthenticationToken(dataAuthUser.user(),dataAuthUser.pass());
-		authenticationManager.authenticate(token);
-		return ResponseEntity.ok().build();
+	public ResponseEntity authUser(@RequestBody @Valid DataAuthUser dataAuthUser) {
+		Authentication authToken = new UsernamePasswordAuthenticationToken(dataAuthUser.user(),dataAuthUser.pass());
+		var authenticatedUser = authenticationManager.authenticate(authToken);
+		var JWTtoken = tokenService.generateToken((User) authenticatedUser.getPrincipal());
+		return ResponseEntity.ok(new DataJWTToken(JWTtoken));
 	}
 
 }
