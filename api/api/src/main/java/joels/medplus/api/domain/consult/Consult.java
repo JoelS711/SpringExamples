a0478d2680f0c578460ceae8681ab6d95ade9c8f
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-05 13:47:09 -0500

    Create Consult.java

---
+++ api/api/src/main/java/joels/medplus/api/domain/consult/Consult.java
@@ -0,0 +1,42 @@
+package joels.medplus.api.domain.consult;
+
+import java.time.LocalDateTime;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import joels.medplus.api.domain.doctor.Doctor;
+import joels.medplus.api.domain.patient.Patient;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name="consults")
+@Entity(name="consult")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Consult {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="doctor_id")
+	private Doctor doctor;
+	
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name="paltient_id")
+	private Patient patient;
+	
+	private LocalDateTime date;
+	
+}
