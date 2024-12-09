9dadb663cddfaa3c61275c14467ed40fcb33e075
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-09 10:15:08 -0500

    Update DataReserveConsult.java

--- api/api/src/main/java/joels/medplus/api/domain/consult/DataReserveConsult.java
+++ api/api/src/main/java/joels/medplus/api/domain/consult/DataReserveConsult.java
@@ -4,7 +4,8 @@ import java.time.LocalDateTime;
 
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotNull;
+import joels.medplus.api.domain.doctor.Speciality;
 
-public record DataReserveConsult(Long idDoctor, @NotNull Long idPatient, @NotNull @Future LocalDateTime date) {
+public record DataReserveConsult(Long idDoctor, @NotNull Long idPatient, @NotNull @Future LocalDateTime date, Speciality speciality) {
 
 }
