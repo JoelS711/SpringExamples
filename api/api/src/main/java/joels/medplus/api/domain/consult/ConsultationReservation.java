ec85a77cf4c46f3dbdbaf9af5cb5e2e1b41fa049
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-08 22:31:28 -0500

    Update ConsultationReservation.java

--- api/api/src/main/java/joels/medplus/api/domain/consult/ConsultationReservation.java
+++ api/api/src/main/java/joels/medplus/api/domain/consult/ConsultationReservation.java
@@ -1,5 +1,28 @@
 package joels.medplus.api.domain.consult;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import joels.medplus.api.domain.doctor.DoctorRepository;
+import joels.medplus.api.domain.patient.PatientRepository;
+
+@Service
 public class ConsultationReservation {
 
+	@Autowired
+	private DoctorRepository doctorRepository;
+
+	@Autowired
+	private PatientRepository patientRepository;
+	
+	@Autowired
+	private ConsultRepository consultRepository;
+	
+	public void reservation(DataReserveConsult data) {
+	
+		var doctor = doctorRepository.findById(data.idDoctor()).get();
+		var patient = patientRepository.findById(data.idPatient()).get();
+		var consult = new Consult(null, doctor, patient, data.date());
+		consultRepository.save(consult);
+	}
 }
