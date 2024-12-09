d7f2065e0fa91f82bbf0836b7c9228f234a910c1
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-08 22:30:47 -0500

    Update ConsultController.java

--- api/api/src/main/java/joels/medplus/api/controller/ConsultController.java
+++ api/api/src/main/java/joels/medplus/api/controller/ConsultController.java
@@ -1,5 +1,6 @@
 package joels.medplus.api.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,16 +9,21 @@ import org.springframework.web.bind.annotation.RestController;
 
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
+import joels.medplus.api.domain.consult.ConsultationReservation;
 import joels.medplus.api.domain.consult.DataDetailConsult;
 import joels.medplus.api.domain.consult.DataReserveConsult;
 
 @RestController
 @RequestMapping("consults")
 public class ConsultController {
+	
+	@Autowired
+	private ConsultationReservation reservation;
 
 	@PostMapping
 	@Transactional
 	public ResponseEntity reserve(@RequestBody @Valid DataReserveConsult data) {
+		reservation.reservation(data);
 		return ResponseEntity.ok(new DataDetailConsult(null, null, null, null));
 	}
 }
