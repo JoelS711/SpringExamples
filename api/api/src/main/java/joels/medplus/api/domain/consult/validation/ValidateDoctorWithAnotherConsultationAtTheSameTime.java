da524d7d250bfff94bfbbfd89eacd2f24f336dcb
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-11 12:59:42 -0500

    Update class ValidateDoctorWithAnotherConsultationAtTheSameTime.java

--- api/api/src/main/java/joels/medplus/api/domain/consult/validation/ValidateDoctorWithAnotherConsultationAtTheSameTime.java
+++ api/api/src/main/java/joels/medplus/api/domain/consult/validation/ValidateDoctorWithAnotherConsultationAtTheSameTime.java
@@ -1,11 +1,16 @@
 package joels.medplus.api.domain.consult.validation;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
 import joels.medplus.api.domain.ValidaException;
 import joels.medplus.api.domain.consult.ConsultRepository;
 import joels.medplus.api.domain.consult.DataReserveConsult;
 
-public class ValidateDoctorWithAnotherConsultationAtTheSameTime {
+@Component
+public class ValidateDoctorWithAnotherConsultationAtTheSameTime implements ValidateOfConsult{
 
+	@Autowired
 	private ConsultRepository consultRepository;
 	
 	public void validate(DataReserveConsult data) {
