126b2a9195ab35d22770213b8a574b1ca222547f
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-11 12:42:28 -0500

    Create class ValidateOfConsult.java

---
+++ api/api/src/main/java/joels/medplus/api/domain/consult/validation/ValidateOfConsult.java
@@ -0,0 +1,8 @@
+package joels.medplus.api.domain.consult.validation;
+
+import joels.medplus.api.domain.consult.DataReserveConsult;
+
+public interface ValidateOfConsult {
+
+	void validate(DataReserveConsult data);
+}
