104d470be7605c9f408f38b1680ab2e0d7c6bd1d
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-10 21:16:54 -0500

    Update class ConsultRepository.java

--- api/api/src/main/java/joels/medplus/api/domain/consult/ConsultRepository.java
+++ api/api/src/main/java/joels/medplus/api/domain/consult/ConsultRepository.java
@@ -1,8 +1,15 @@
 package joels.medplus.api.domain.consult;
 
+import java.time.LocalDateTime;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ConsultRepository extends JpaRepository <Consult, Long>{
+
+	boolean existsByPatientIdAndDateBetween(Long idPatient, LocalDateTime firstSchedule,
+			LocalDateTime lastSchedule);
+
+	boolean existsByDoctorIdAndDate(Long idDoctor,LocalDateTime date);
 
 	
 }
