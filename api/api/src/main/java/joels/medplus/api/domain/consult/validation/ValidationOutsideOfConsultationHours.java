e47f50a3b315f02dec8031fecdcdc6ef6337dfc3
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-10 21:17:28 -0500

    Update class ValidationOutsideOfConsultationHours.java

--- api/api/src/main/java/joels/medplus/api/domain/consult/validation/ValidationOutsideOfConsultationHours.java
+++ api/api/src/main/java/joels/medplus/api/domain/consult/validation/ValidationOutsideOfConsultationHours.java
@@ -1,5 +1,19 @@
 package joels.medplus.api.domain.consult.validation;
 
+import java.time.DayOfWeek;
+
+import joels.medplus.api.domain.ValidaException;
+import joels.medplus.api.domain.consult.DataReserveConsult;
+
 public class ValidationOutsideOfConsultationHours {
 
+	public void validate(DataReserveConsult data) {
+		var consultationDate = data.date();
+		var sunday = consultationDate.getDayOfWeek().equals(DayOfWeek.SUNDAY);
+		var hoursBeforeOpening = consultationDate.getHour() < 7;
+		var afterClosingHours = consultationDate.getHour() > 18;
+		if(sunday || hoursBeforeOpening || afterClosingHours) {
+			throw new ValidaException("Selected time outside of service hours");
+		}
+	}
 }
