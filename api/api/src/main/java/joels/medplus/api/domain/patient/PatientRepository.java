b13ba179510f75aeb907af00f3a563b897590677
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-10 21:18:19 -0500

    Update class PatientRepository.java

--- api/api/src/main/java/joels/medplus/api/domain/patient/PatientRepository.java
+++ api/api/src/main/java/joels/medplus/api/domain/patient/PatientRepository.java
@@ -5,7 +5,11 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import jakarta.validation.constraints.NotNull;
+
 public interface PatientRepository extends JpaRepository<Patient, Long>{
 
 	Page<Patient> findAllByActiveTrue(Pageable pagination);
+
+	boolean findActiveById(Long idPatient);
 }
