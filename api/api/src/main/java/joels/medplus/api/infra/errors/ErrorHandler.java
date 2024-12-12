13452c35b3f0e0783674cd16e4697d6fb6eaa097
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-12 09:42:54 -0500

    Add method tryErrorValidation

--- api/api/src/main/java/joels/medplus/api/infra/errors/ErrorHandler.java
+++ api/api/src/main/java/joels/medplus/api/infra/errors/ErrorHandler.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import jakarta.persistence.EntityNotFoundException;
+import joels.medplus.api.domain.ValidaException;
 
 @RestControllerAdvice
 public class ErrorHandler {
@@ -20,6 +21,11 @@ public class ErrorHandler {
 	public ResponseEntity tryError404(MethodArgumentNotValidException e) {
 		var errors = e.getFieldErrors().stream().map(DataErrorValidation::new).toList();
 		return ResponseEntity.badRequest().body(errors);
+	}
+	
+	@ExceptionHandler(ValidaException.class)
+	public ResponseEntity tryErrorValidation(ValidaException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
 	}
 	
 	private record DataErrorValidation(String field, String error) {
