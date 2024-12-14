d8228ec93876db55100dbffc1b6a288e9d43ffb2
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-14 13:34:37 -0500

    Update class ConsultController.java

--- api/api/src/main/java/joels/medplus/api/controller/ConsultController.java
+++ api/api/src/main/java/joels/medplus/api/controller/ConsultController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import joels.medplus.api.domain.consult.ConsultationReservation;
@@ -14,7 +15,8 @@ import joels.medplus.api.domain.consult.DataDetailConsult;
 import joels.medplus.api.domain.consult.DataReserveConsult;
 
 @RestController
-@RequestMapping("consults")
+@RequestMapping("/consults")
+@SecurityRequirement(name = "bearer-key")
 public class ConsultController {
 	
 	@Autowired
