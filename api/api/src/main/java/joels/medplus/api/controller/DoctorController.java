780e120b3b3c51a5e02a36ccde8dd1ff3a954d04
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-14 13:36:00 -0500

    Update class DoctorController.java

--- api/api/src/main/java/joels/medplus/api/controller/DoctorController.java
+++ api/api/src/main/java/joels/medplus/api/controller/DoctorController.java
@@ -18,6 +18,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.transaction.Transactional;
 import jakarta.validation.Valid;
 import joels.medplus.api.domain.address.DataAddress;
@@ -30,6 +31,7 @@ import joels.medplus.api.domain.doctor.UpdateDoctorData;
 
 @RestController
 @RequestMapping("/doctor")
+@SecurityRequirement(name = "bearer-key")
 public class DoctorController {
 
 	@Autowired
