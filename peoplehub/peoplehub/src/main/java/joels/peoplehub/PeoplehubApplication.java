a8918b2c99f64a7ee0446835a0dc1314870d299d
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-17 12:19:37 -0500

    Create project peoplehub

---
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/PeoplehubApplication.java
@@ -0,0 +1,13 @@
+package joels.peoplehub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PeoplehubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PeoplehubApplication.class, args);
+	}
+
+}
---
+++ peoplehub/peoplehub/src/test/java/joels/peoplehub/PeoplehubApplicationTests.java
@@ -0,0 +1,13 @@
+package joels.peoplehub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PeoplehubApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
