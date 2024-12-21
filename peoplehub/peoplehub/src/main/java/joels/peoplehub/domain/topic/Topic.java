6e181dc22bfdb730264967cf5b32881cfdc18fc9
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-21 09:41:18 -0500

    Update Topic.jaava

--- peoplehub/peoplehub/src/main/java/joels/peoplehub/domain/topic/Topic.java
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/domain/topic/Topic.java
@@ -1,17 +1,27 @@
 package joels.peoplehub.domain.topic;
 
+import java.time.LocalDateTime;
+
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
+import joels.peoplehub.domain.answer.Answer;
+import joels.peoplehub.domain.user.User;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
-@Table(name="topics")
-@Entity(name="Topic")
+@Table(name = "topics")
+@Entity(name = "Topic")
 @Getter
 @NoArgsConstructor
 @AllArgsConstructor
@@ -21,6 +31,24 @@ public class Topic {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
-	
+
+	private String title;
+
+	private String message;
+
+	private LocalDateTime creationDate;
+
+	private Boolean status;
+
+	@ManyToOne(fetch = FetchType.LAZY)
+	@JoinColumn(name = "user_id")
+	private User user;
+
+	@Enumerated(EnumType.STRING)
+	private Course course;
+
+	@OneToMany(fetch = FetchType.LAZY)
+	@JoinColumn(name = "answer_id")
+	private Answer answer;
+
 }
