6e9eae470bb15a54f464ae0bb5dcbb54ac278156
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-23 12:18:07 -0500

    Update migration V3

--- peoplehub/peoplehub/src/main/resources/db/migration/V3__create-table-answers.sql
+++ peoplehub/peoplehub/src/main/resources/db/migration/V3__create-table-answers.sql
@@ -1,6 +1,10 @@
-CREATE TABLE users (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    name VARCHAR(255) NOT NULL,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL
+CREATE TABLE answers (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    message TEXT NOT NULL,
+    topic_id BIGINT NOT NULL,
+    creation_date DATETIME NOT NULL,
+    user_id BIGINT NOT NULL,
+    solution BOOLEAN NOT NULL,
+    CONSTRAINT fk_answers_topic FOREIGN KEY (topic_id) REFERENCES topics (id) ON DELETE CASCADE,
+    CONSTRAINT fk_answers_user FOREIGN KEY (user_id) REFERENCES users (id) ON DELETE CASCADE
 );
