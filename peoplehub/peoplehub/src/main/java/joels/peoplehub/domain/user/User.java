e2a2559b1c4b6826b9dc951b1548a55903d473a0
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-23 12:27:02 -0500

    Add Override to class User.java

--- peoplehub/peoplehub/src/main/java/joels/peoplehub/domain/user/User.java
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/domain/user/User.java
@@ -48,24 +48,37 @@ public class User implements UserDetails{
 	@OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
 	private List<Answer> answers = new ArrayList<>();
 	
+	@Override
 	public Collection<? extends GrantedAuthority> getAuthorities() {
 		return List.of(new SimpleGrantedAuthority("ROLE_USER"));
 	}
+	
+	@Override
 	public String getPassword() {
 		return password;
 	}
+	
+	@Override
 	public String getUsername() {
 		return email;
 	}
+	
+	@Override
 	public boolean isEnabled() {
 		return true;
 	}
+	
+	@Override
 	public boolean isCredentialsNonExpired() {
 		return true;
 	}
+	
+	@Override
 	public boolean isAccountNonLocked() {
 		return true;
 	}
+	
+	@Override
 	public boolean isAccountNonExpired() {
 		return true;
 	}
