d002ae39452af6039204deeb0a431fe14991dcea
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-27 12:44:03 -0500

    Create class TopicController

---
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/controller/TopicController.java
@@ -0,0 +1,25 @@
+package joels.peoplehub.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import joels.peoplehub.domain.answer.AnswerRepository;
+import joels.peoplehub.domain.topic.TopicRepository;
+import joels.peoplehub.domain.user.UserRepository;
+
+@RestController
+@RequestMapping("/topics/{topicId}")
+@SecurityRequirement(name = "bearer-key")
+public class TopicController {
+
+	@Autowired
+	private UserRepository userRespository;
+	
+	@Autowired
+	private TopicRepository topicRespository;
+	
+	@Autowired
+	private AnswerRepository answerRespository;
+}
