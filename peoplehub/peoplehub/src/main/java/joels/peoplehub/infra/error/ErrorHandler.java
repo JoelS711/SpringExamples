017c51e9c174657fd764200bcff01b27de6e83bb
Emanuell Gelvez <joelsanabria11@gmail.com>
2024-12-29 09:59:30 -0500

    Create class ErrorHandle.java and ValidaException.java

---
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/infra/error/ErrorHandler.java
@@ -0,0 +1,35 @@
+package joels.peoplehub.infra.error;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class ErrorHandler {
+
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity tryError404() {
+		return ResponseEntity.notFound().build();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity tryError404(MethodArgumentNotValidException e) {
+		var errors = e.getFieldErrors().stream().map(DataErrorValidation::new).toList();
+		return ResponseEntity.badRequest().body(errors);
+	}
+	
+	@ExceptionHandler(ValidaException.class)
+	public ResponseEntity tryErrorValidation(ValidaException e) {
+		return ResponseEntity.badRequest().body(e.getMessage());
+	}
+	
+	private record DataErrorValidation(String field, String error) {
+		public DataErrorValidation(FieldError error) {
+			this(error.getField(), error.getDefaultMessage());
+		}
+	}
+}
---
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/infra/error/ValidaException.java
@@ -0,0 +1,8 @@
+package joels.peoplehub.infra.error;
+
+public class ValidaException extends RuntimeException {
+
+	public ValidaException(String message) {
+		super(message);
+	}
+}
