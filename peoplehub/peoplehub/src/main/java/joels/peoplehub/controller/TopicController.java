3d4f04c86e3563000d9030c6ddac5fa32d03590e
Emanuell Gelvez <joelsanabria11@gmail.com>
2025-01-02 00:01:24 -0500

    Update class TopicController.java

--- peoplehub/peoplehub/src/main/java/joels/peoplehub/controller/TopicController.java
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/controller/TopicController.java
@@ -1,12 +1,21 @@
 package joels.peoplehub.controller;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponentsBuilder;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import jakarta.validation.Valid;
 import joels.peoplehub.domain.answer.AnswerRepository;
+import joels.peoplehub.domain.topic.DataNewTopic;
 import joels.peoplehub.domain.topic.TopicRepository;
+import joels.peoplehub.domain.topic.ValidationTopic;
 import joels.peoplehub.domain.user.UserRepository;
 
 @RestController
@@ -22,4 +31,14 @@ public class TopicController {
 	
 	@Autowired
 	private AnswerRepository answerRespository;
+	
+	@Autowired
+	private ValidationTopic validation;
+	
+	@PostMapping
+	public ResponseEntity<DataNewTopic> newTopic(@RequestBody @Valid DataNewTopic data, UriComponentsBuilder uriComponentsBuilder){
+		DataNewTopic dataNewTopic = validation.postTopic(data);
+		URI url = uriComponentsBuilder.path("/topics/{id}").buildAndExpand(dataNewTopic.userId()).toUri();
+		return ResponseEntity.created(url).body(dataNewTopic);
+	}
 }
