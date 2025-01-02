a9a4f6f4bce37c01c0a8d7e11c3e6a249b16acde
Emanuell Gelvez <joelsanabria11@gmail.com>
2025-01-02 00:01:53 -0500

    Update record DataNewTopic.java

--- peoplehub/peoplehub/src/main/java/joels/peoplehub/domain/topic/DataNewTopic.java
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/domain/topic/DataNewTopic.java
@@ -1,12 +1,14 @@
 package joels.peoplehub.domain.topic;
 
+import java.time.LocalDateTime;
+
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 
-public record DataNewTopic(@NotNull Long userId,@NotBlank String title,@NotBlank String message,@NotNull  Course course) {
+public record DataNewTopic(@NotNull Long userId,@NotBlank String title,@NotBlank String message,@NotNull  Course course, LocalDateTime creationDate) {
 
 	public DataNewTopic(Topic topic) {
-		this(topic.getId(), topic.getTitle(), topic.getMessage(), topic.getCourse());
+		this(topic.getId(), topic.getTitle(), topic.getMessage(), topic.getCourse(), topic.getCreationDate());
 	}
 
 }
