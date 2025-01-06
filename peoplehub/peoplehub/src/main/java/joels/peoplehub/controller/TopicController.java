01fb2b8b6752c85b1c0bd5a98beac0c35cfcb524
Emanuell Gelvez <joelsanabria11@gmail.com>
2025-01-06 08:50:19 -0500

    Update class TopicController.java

--- peoplehub/peoplehub/src/main/java/joels/peoplehub/controller/TopicController.java
+++ peoplehub/peoplehub/src/main/java/joels/peoplehub/controller/TopicController.java
@@ -3,17 +3,26 @@ package joels.peoplehub.controller;
 import java.net.URI;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
+import org.springframework.data.web.SortDefault;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.util.UriComponentsBuilder;
-
+import org.springframework.data.domain.Sort;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import joels.peoplehub.domain.answer.AnswerRepository;
 import joels.peoplehub.domain.topic.DataNewTopic;
+import joels.peoplehub.domain.topic.DataUpdateTopic;
 import joels.peoplehub.domain.topic.TopicRepository;
 import joels.peoplehub.domain.topic.ValidationTopic;
 import joels.peoplehub.domain.user.UserRepository;
@@ -25,20 +34,35 @@ public class TopicController {
 
 	@Autowired
 	private UserRepository userRespository;
-	
+
 	@Autowired
-	private TopicRepository topicRespository;
-	
+	private TopicRepository topicRepository;
+
 	@Autowired
 	private AnswerRepository answerRespository;
-	
+
 	@Autowired
 	private ValidationTopic validation;
-	
+
 	@PostMapping
-	public ResponseEntity<DataNewTopic> newTopic(@RequestBody @Valid DataNewTopic data, UriComponentsBuilder uriComponentsBuilder){
+	public ResponseEntity<DataNewTopic> newTopic(@RequestBody @Valid DataNewTopic data,
+			UriComponentsBuilder uriComponentsBuilder) {
 		DataNewTopic dataNewTopic = validation.postTopic(data);
 		URI url = uriComponentsBuilder.path("/topics/{id}").buildAndExpand(dataNewTopic.userId()).toUri();
 		return ResponseEntity.created(url).body(dataNewTopic);
+	}
+
+	@GetMapping
+	public ResponseEntity<Page<DataNewTopic>> listTopics(
+			@PageableDefault @SortDefault(sort = "status", direction = Sort.Direction.ASC) @SortDefault(sort = "id", direction = Sort.Direction.ASC) Pageable pageable) {
+
+		return ResponseEntity.ok(topicRepository.findAll(pageable).map(DataNewTopic::new));
+	}
+	
+	@PutMapping("/{id}")
+    @Transactional
+    public ResponseEntity<DataUpdateTopic> updateTopic(@PathVariable("id") Long id, @RequestBody @Valid DataUpdateTopic dataUpdate){
+		DataNewTopic updatedData = validation.updateTopic(id, dataUpdate);
+		return ResponseEntity.ok(dataUpdate);
 	}
 }
